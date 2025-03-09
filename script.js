dffea04d9d2ed9f05b4c5c5ee15d2b7b72de9685
Ayushrai9142 <ayushrai7533@gmail.com>
2025-03-09 12:25:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const secretElement = document.getElementById("secret");
 
     revealButton.addEventListener("click", function() {
-        secretElement.innerHTML = "Ishika, I really love you! You don't know how much I love you! ❤️<br> Just like a cherry on top of a cake, you make my life complete! 🍒";
+        secretElement.innerHTML = " I really love you! You don't know how much I love you! ❤️<br> Just like a cherry on top of a cake, you make my life complete! 🍒";
         secretElement.style.opacity = "1"; // Message visible hoga
         revealButton.style.display = "none"; // Button hide hoga
     });
